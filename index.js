255f26587a16dc10c1282d319290697e5739092f
shel1977 <56795225+shel1977@users.noreply.github.com>
2020-04-12 14:12:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000/']}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
