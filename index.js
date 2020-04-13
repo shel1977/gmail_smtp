d5ab14442bffe6ec5e8a881169f184702c60cc27
shel1977 <56795225+shel1977@users.noreply.github.com>
2020-04-13 12:41:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: ['https://shel1977.github.io/portfolio/']}));
+    origin: ['https://shel1977.github.io/portfolio']}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
